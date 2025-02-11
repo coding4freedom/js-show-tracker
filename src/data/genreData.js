c966f4cff0f08fe608e7139b5b83551309f9fab6
coding4freedom <chawkins2010@hotmail.com>
2025-02-11 01:40:44 -0600

    genre data file added

---
+++ src/data/genreData.js
@@ -0,0 +1,17 @@
+
+const genre = [
+    {
+        id: 'tv',
+        genres: ['action', 'comedy', 'drama', 'sci-fi', 'horror', 'romance', 'fantasy', 'western'],
+    },
+    {
+        id: 'movies',
+        genres: ['action', 'comedy', 'drama', 'sci-fi', 'horror', 'romance', 'fantasy', 'western', 'sports', 'musical', 'documentary', 'thriller', 'war', 'mystery'],
+    },
+    {
+        id: 'anime',
+        genres: ['action', 'adventure', 'comedy', 'drama', 'ecchi', 'fantasy', 'horror', 'isekai', 'magic', 'mecha', 'mystery', 'romance', 'sci-fi', 'shounen', 'slice-of-life', 'sports', 'space', 'harem', 'music', 'game'],
+    },
+];
+
+export default genre;
